10ee282fe0312474976f4331fc5f6ad43f6c0d13
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2017-06-01 18:18:13 +0900

    inject設定

--- Gothic/app/src/main/java/zukkey/gothic/ui/activity/fragment/BaseFragment.kt
+++ Gothic/app/src/main/java/zukkey/gothic/ui/activity/fragment/BaseFragment.kt
@@ -1,14 +1,22 @@
 package zukkey.gothic.ui.activity.fragment
 
 import android.app.Fragment
+import android.os.Bundle
+import zukkey.gothic.GoThicApplication
 import zukkey.gothic.ui.activity.BaseActivity
 
 /**
  * Created by YUTARO SUZUKI on 2017/05/27.
- * All Fragment base class
+ * All Fragment base class (DI)
  */
 
 abstract class BaseFragment: Fragment() {
+  /**
+   * get current Activity
+   */
   val baseActivity: BaseActivity get() { return activity as BaseActivity }
-
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GoThicApplication.mainComponent.inject(this)
+  }
 }
--- Gothic/app/src/main/java/zukkey/gothic/ui/activity/viewholder/BaseViewHolder.kt
+++ Gothic/app/src/main/java/zukkey/gothic/ui/activity/viewholder/BaseViewHolder.kt
@@ -2,10 +2,15 @@ package zukkey.gothic.ui.activity.viewholder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import zukkey.gothic.GoThicApplication
 
 /**
  * Created by zukkey on 2017/06/01.
+ * base viewholder for injection
  */
 abstract class BaseViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+  init {
+    GoThicApplication.mainComponent.inject(this)
+  }
 }
