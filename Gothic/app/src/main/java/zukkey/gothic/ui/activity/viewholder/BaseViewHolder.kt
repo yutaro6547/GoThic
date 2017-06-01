d09ab4e0c66c2572bbaba8ddb9834ea5794809ec
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2017-06-01 18:10:46 +0900

    ベースとなるviewHolderとホーム画面のViewHolderを追加

---
+++ Gothic/app/src/main/java/zukkey/gothic/ui/activity/viewholder/BaseViewHolder.kt
@@ -0,0 +1,11 @@
+package zukkey.gothic.ui.activity.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Created by zukkey on 2017/06/01.
+ */
+abstract class BaseViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+}
---
+++ Gothic/app/src/main/java/zukkey/gothic/ui/activity/viewholder/HomeViewHolder.kt
@@ -0,0 +1,5 @@
+package zukkey.gothic.ui.activity.viewholder
+
+/**
+ * Created by zukkey on 2017/06/01.
+ */
