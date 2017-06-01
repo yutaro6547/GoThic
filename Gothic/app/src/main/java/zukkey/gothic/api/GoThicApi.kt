d5381274552e3ce772ed19a4f1eb694986570354
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2017-06-01 18:41:07 +0900

    ベースとなるAPIとServiceを作成

---
+++ Gothic/app/src/main/java/zukkey/gothic/api/GoThicApi.kt
@@ -0,0 +1,11 @@
+package zukkey.gothic.api
+
+/**
+ * Created by zukkey on 2017/06/01.
+ * All interface for GoThic api
+ */
+interface GoThicApi {
+  companion object {
+    val START_PAGE = 1
+  }
+}
---
+++ Gothic/app/src/main/java/zukkey/gothic/api/GoThicService.kt
@@ -0,0 +1,12 @@
+package zukkey.gothic.api
+
+import zukkey.gothic.GoThicApplication
+
+/**
+ * Created by zukkey on 2017/06/01.
+ * GoThic Service to connect server
+ */
+
+class GoThicService(val application: GoThicApplication) {
+
+}
